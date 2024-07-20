221dab6c79ab10f255d02631a43b6cc108abdac9
MarianaOlvr11 <marxvitoria@hotmail.com>
2024-07-20 17:26:31 -0300

    Primeiro Commit

---
+++ src/test/java/com/mavioliveira/workshopMongoDB/WorkshopMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mavioliveira.workshopMongoDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopMongoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
